b280f50f69818dbd4c5d6e8bcd075a49fc205493
Satyam Chaudhary <chaudharysatyam313@gmail.com>
2023-05-30 09:38:05 +0530

    second commit

--- sum/sum.java
+++ sum/sum.java
@@ -3,6 +3,6 @@ public class sum {
     public static void main(String[] args) {
         int a = 4;
         int b = 5;
-        System.out.println(a + b);
+        System.out.println("the sum is " +(a + b));
     }
 }
